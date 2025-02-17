edccd4100487517a76278661a4f372fef21375c1
GustavoLaraa <m.gustavo_@hotmail.com>
2025-02-17 17:22:27 -0300

    Update ingressoRoutes.js

--- ingressoRoutes.js
+++ ingressoRoutes.js
@@ -4,19 +4,16 @@ const ingressoController = require('../controllers/ingressoController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const adminMiddleware = require('../middlewares/adminMiddleware');
 
-// Verifica se o controlador está carregado corretamente
-console.log("Ingresso Controller:", ingressoController);
+console.log("🔍 Rotas de Ingressos carregadas!");
 
-// Rotas públicas
+// Rotas de ingressos
 router.get('/ingressos', ingressoController.listarIngressos);
-
-// Rotas protegidas (apenas usuários autenticados)
-router.post('/comprar', authMiddleware, ingressoController.comprarIngresso);
-router.get('/historico', authMiddleware, ingressoController.historicoCompras);
-
-// Rotas administrativas (apenas administradores)
 router.post('/ingressos', authMiddleware, adminMiddleware, ingressoController.criarIngresso);
 router.put('/ingressos/:id', authMiddleware, adminMiddleware, ingressoController.atualizarIngresso);
 router.delete('/ingressos/:id', authMiddleware, adminMiddleware, ingressoController.deletarIngresso);
+
+// Rotas de compra e histórico
+router.post('/comprar', authMiddleware, ingressoController.comprarIngresso);
+router.get('/ingressos/historico', authMiddleware, ingressoController.historicoCompras);
 
 module.exports = router;
